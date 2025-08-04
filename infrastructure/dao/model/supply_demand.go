37d8cea17bad85f139f7519a891ffbc3ab8304a9
A-outsider <chen15360217390@qq.com>
2025-08-04 17:26:20 +0800

    refactor: 优化数据库实体

--- infrastructure/dao/model/supply_demand.go
+++ infrastructure/dao/model/supply_demand.go
@@ -1,28 +1,27 @@
 package model
 
 import (
-	"gorm.io/datatypes"
 	"gorm.io/gorm"
 	"time"
 )
 
 type SupplyDemand struct {
-	ID       uint   `gorm:"primaryKey;autoIncrement;comment:供需ID" json:"id"`
-	Title    string `gorm:"type:varchar(255);not null;comment:标题" json:"title"`
-	Content  string `gorm:"type:text;comment:内容" json:"content"`
-	TagWeigh string `gorm:"type:varchar(255);default:0;comment:标签" j重量son:"tag_weigh"`
-	TagName  string `gorm:"type:varchar(255);default:'';comment:标签名称" json:"tag_name"`
-	TagPrice string `gorm:"type:varchar(255);default:'';comment:标签价格" json:"tag_price"`
+	BaseModel
 
-	CoverURL string         `gorm:"type:varchar(512);default:'';comment:封面图地址" json:"cover_url"`
-	FilesURL datatypes.JSON `gorm:"type:json;comment:附件地址列表" json:"files_url"`
-	Likes    int            `gorm:"type:int;default:0;comment:点赞数" json:"like"`
+	Title    string   `gorm:"type:varchar(255);not null;comment:标题" json:"title"`
+	Content  string   `gorm:"type:text;comment:内容" json:"content"`
+	CoverURL string   `gorm:"type:varchar(512);default:'';comment:封面图地址" json:"cover_url"`
+	FilesURL []string `gorm:"serializer:json;type:json;comment:附件地址列表" json:"files_url"`
+	Likes    int      `gorm:"type:int;default:0;comment:点赞数" json:"likes"`
 
-	UserID uint `gorm:"index;comment:用户id" json:"user_id"`
+	Tag    TagInfo `gorm:"embedded;embeddedPrefix:tag_" json:"tag"`
+	UserID uint    `gorm:"index;comment:用户id" json:"user_id"`
+}
 
-	CreatedAt time.Time      `gorm:"autoCreateTime;comment:创建时间" json:"created_at"`
-	UpdatedAt time.Time      `gorm:"autoUpdateTime;comment:更新时间" json:"updated_at"`
-	DeletedAt gorm.DeletedAt `gorm:"index;comment:删除时间" json:"-"`
+type TagInfo struct {
+	Weigh string `gorm:"type:varchar(255);default:0;comment:标签" json:"weigh"`
+	Name  string `gorm:"type:varchar(255);default:'';comment:标签名称" json:"name"`
+	Price string `gorm:"type:varchar(255);default:'';comment:标签价格" json:"price"`
 }
 
 type SupplyDemandComment struct {
